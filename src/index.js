91be1ec7c929b12d89e209737115577af3895841
Perry Tran <ptran209@gmail.com>
2018-09-12 16:42:55 -0700

    React intro challenge

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+const user = {
+    firstName: 'Perry',
+    lastName: 'Tran',
+}
+
+function fullName(user) {
+    return `${user.firstName} ${user.lastName}`; 
+}
+
+const greeting = <h1>Hello {fullName(user)}!</h1>;
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
+
